0edfffb2505fc7f7ee4b365fb83aee533ba066df
Andrej Katin <katin.andrej96@gmail.com>
2022-05-13 13:08:41 +0200

    Add project files.

---
+++ OTS2022_ApartmentAgency/Models/Apartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2022_ApartmentAgency.Models
+{
+    public class Apartment
+    {
+        public ApartmentType ReserveApartment(string floor, int beds, bool petsAllowed, bool terrace, bool kitchen, int numberOfDays)
+        {
+            return 0;
+        }
+    }
+}
---
+++ OTS2022_ApartmentAgency/Models/ApartmentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2022_ApartmentAgency.Models
+{
+    public enum ApartmentType
+    {
+        Complex_A,
+        Complex_B,
+        Complex_C
+    }
+}
